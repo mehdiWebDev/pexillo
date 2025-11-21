65c2e643d8ca3ffec04f272805d4431e8c5b7c2a
Mahdi ouatah <mahdi@nixa.ca>
2025-11-20 23:47:50 -0500

    fix lint errors

--- src/types/google-maps.d.ts
+++ src/types/google-maps.d.ts
@@ -1,5 +1,15 @@
-declare namespace JSX {
-    interface IntrinsicElements {
-        'gmp-place-autocomplete': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+import React from 'react';
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'gmp-place-autocomplete': React.DetailedHTMLProps<
+                React.HTMLAttributes<HTMLElement> & {
+                    'included-primary-types'?: string;
+                    placeholder?: string;
+                },
+                HTMLElement
+            >;
+        }
     }
 }
